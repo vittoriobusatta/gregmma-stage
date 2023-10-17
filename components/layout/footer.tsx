fe59927f09819f4242069385342895867f9095a6
Vittorio <tonibusatta@gmail.com>
2023-10-17 16:40:25 +0400

    init

---
+++ components/layout/footer.tsx
@@ -0,0 +1,20 @@
+const { COMPANY_NAME, SITE_NAME } = process.env;
+
+export default async function Footer() {
+  const currentYear = new Date().getFullYear();
+  const copyrightDate = 2023 + (currentYear > 2023 ? `-${currentYear}` : '');
+  const copyrightName = COMPANY_NAME || SITE_NAME || '';
+
+  return (
+    <footer className="text-sm text-neutral-500 dark:text-neutral-400">
+      <div className="border-t border-neutral-200 py-6 text-sm dark:border-neutral-700">
+        <div className="mx-auto flex w-full max-w-7xl flex-col items-center gap-1 px-4 md:flex-row md:gap-0 md:px-4 min-[1320px]:px-0">
+          <p>
+            &copy; {copyrightDate} {copyrightName}
+            {copyrightName.length && !copyrightName.endsWith('.') ? '.' : ''} All rights reserved.
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+}
---
+++ components/layout/navbar/index.tsx
@@ -0,0 +1,37 @@
+import Cart from 'components/cart';
+import OpenCart from 'components/cart/open-cart';
+import LogoSquare from 'components/logo-square';
+import { getMenu } from 'lib/shopify';
+import { Menu } from 'lib/shopify/types';
+import Link from 'next/link';
+import { Suspense } from 'react';
+import MobileMenu from './mobile-menu';
+const { SITE_NAME } = process.env;
+
+export default async function Navbar() {
+  const menu = await getMenu('next-js-frontend-header-menu');
+  console.log(menu);
+
+  return (
+    <nav className="relative flex items-center justify-between p-4 lg:px-6">
+      <div className="block flex-none md:hidden">
+        <MobileMenu menu={menu} />
+      </div>
+      <div className="flex w-full items-center">
+        <div className="flex w-full md:w-1/3">
+          <Link href="/" className="mr-2 flex w-full items-center justify-center md:w-auto lg:mr-6">
+            <LogoSquare />
+            <div className="ml-2 flex-none text-sm font-medium uppercase md:hidden lg:block">
+              {SITE_NAME}
+            </div>
+          </Link>
+        </div>
+        <div className="flex justify-end md:w-1/3">
+          <Suspense fallback={<OpenCart />}>
+            <Cart />
+          </Suspense>
+        </div>
+      </div>
+    </nav>
+  );
+}
